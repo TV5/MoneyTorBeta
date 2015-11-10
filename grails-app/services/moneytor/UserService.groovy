2eec3da89ed46eb203225dc876310919c851cdfe
Ivan Wesley Chua <ivanwesleychua@yahoo.com>
2015-11-10 11:01:31 +0800

    temp

--- grails-app/services/moneytor/UserService.groovy
+++ grails-app/services/moneytor/UserService.groovy
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:grails-app/services/moneytor/UserService.groovy
 package moneytor
 
 import grails.transaction.Transactional
@@ -32,38 +31,3 @@ class UserService {
 		return adminList
 	}
 }
-=======
-package moneytor
-
-import grails.transaction.Transactional
-
-@Transactional
-class UserService {
-
-    def serviceMethod() {
-
-    }
-	def addUser(user){
-		def userCheck = User.findByUsername(user.username)
-		if(userCheck){
-			return "username already in use"
-		}else{
-		user.save()
-		}
-	}
-	def login(username, password) {
-		def user = User.findByUsernameAndPassword(username, password)
-		return user
-		
-	}
-	def listEmployees(){
-		def empList =User.findAllByStatusAndType(1, "E")
-		return empList
-	}
-	
-	def listAdmins(){
-		def adminList = User.findAllByStatusAndType(1, "A")
-		return adminList
-	}
-}
->>>>>>> master:second/Moneytor/grails-app/services/moneytor/UserService.groovy
