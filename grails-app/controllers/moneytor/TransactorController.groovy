157bd06d21b4c0a096a1d87b7d25770e62aaffb2
dksandimas <dksandimas@gmail.com>
2015-11-13 21:35:40 +0800

    DK -RETRIEVE
    HEHE

--- grails-app/controllers/moneytor/TransactorController.groovy
+++ grails-app/controllers/moneytor/TransactorController.groovy
@@ -2,10 +2,28 @@ package moneytor
 
 class TransactorController {
 
+	def transactorService
     def index() { 
+		def tList = getTransactorList()
+		[transactorList:tList]
+	}
+	def addTransactor() {
+		System.out.println("add transactor")
+		def transactor = new Transactor(
+			name: params.name,
+			address: params.address,
+			telephone_no: params.telephone_no,
+			mobile_no: params.mobile_no,
+			terms: params.terms,
+			type: params.type
+			)
+		transactorService.addTransactor(transactor)
+		
 		
 	}
-	
-	
+	def getTransactorList() {
+		def transactorList = transactorService.getTransactorList()
+		return transactorList
+	}
 	
 }
