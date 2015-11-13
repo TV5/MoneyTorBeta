cd312e98bb6e8d621ef2b63fbb90519cde92da21
dksandimas <dksandimas@gmail.com>
2015-11-13 19:59:34 +0800

    Controllers

---
+++ test/unit/moneytor/TransactorControllerSpec.groovy
@@ -0,0 +1,20 @@
+package moneytor
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(TransactorController)
+class TransactorControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
