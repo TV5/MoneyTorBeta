cd5fa6367a35450fede53672b987c90319194ddc
dksandimas <dksandimas@gmail.com>
2015-11-19 13:47:58 +0800

    s

--- web-app/js/main.js
+++ web-app/js/main.js
@@ -399,3 +399,15 @@
 		document.getElementById('addmore').className = 'ui teal button'; 
 		document.getElementById('saveBtn').value = 'Saved';
 		} 
+	
+	function addmore(){
+		document.getElementById('addmore').className = 'ui button'; 
+		} 
+	
+	function checkSave(){
+		if(document.getElementById('addmore').className == 'ui teal button'){
+			return true;
+		}else{
+			return false;
+		}
+	}
