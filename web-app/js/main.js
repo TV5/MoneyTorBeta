9462a5a96124120f914f20390b2c58d4950019fa
dksandimas <dksandimas@gmail.com>
2015-11-19 13:26:10 +0800

    a

--- web-app/js/main.js
+++ web-app/js/main.js
@@ -393,3 +393,9 @@
 
 		$('#editadministrator').modal('show');
 	}
+	
+
+	function saved(){
+		document.getElementById('addmore').className = 'ui teal button'; 
+		document.getElementById('saveBtn').value = 'Saved';
+		} 
