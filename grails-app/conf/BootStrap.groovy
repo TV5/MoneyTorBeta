51b48a27758c7e8284566e8200362b35fc7145fc
Joseph Gabriell Palmaras <josephpalmaras@gmail.com>
2015-11-24 10:26:49 +0800

    bootstrap
    1 change

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,13 +3,7 @@ import moneytor.*
 class BootStrap {
 
     def init = { servletContext ->
-		User user = new User(username:'adminuser', password:'adminuser', f_name:'Admin', l_name:'Admin', status:'1', type:'A')
-		if (!user.save()){
-			log.error "Could not save user!!"
-			log.error "${user.errors}"
-		  }	
-		Account account = new Account(amount: '999',type: 'P').save()
-		Account account1 = new Account(amount: '999',type: 'R').save()
+		
 	}
     def destroy = {
     }
