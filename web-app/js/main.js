28a6e58ec4420a309efc4a026954b77cca82b8fa
Joseph Gabriell Palmaras <josephpalmaras@gmail.com>
2016-02-12 20:06:50 +0800

    bug fix

--- web-app/js/main.js
+++ web-app/js/main.js
@@ -614,30 +614,6 @@ function editTransactor(name, address, telephone_no, mobile_no, terms) {
 }
 
 function editUserAccount(vid, vf_name, vl_name, vpassword) {
-	$('.ui.form').form({
-		fields : {
-			uF_name : {
-				identifier : 'uF_name',
-				rules : [ {
-					type : 'empty',
-					prompt : 'First Name must not be blank.'
-				}, {
-					type : 'regExp[/^[a-zA-Z .]+$/]',
-					prompt : 'First Name must only contain letters.'
-				} ]
-			},
-			uL_name : {
-				identifier : 'uL_name',
-				rules : [ {
-					type : 'empty',
-					prompt : 'Last Name must not be blank.'
-				}, {
-					type : 'regExp[/^[a-zA-Z .]+$/]',
-					prompt : 'Last Name must only contain letters.'
-				} ]
-			}
-		}
-	});
 	
 	document.getElementById("uId").value = vid;
 	document.getElementById("uF_name").value = vf_name;
@@ -645,8 +621,61 @@ function editUserAccount(vid, vf_name, vl_name, vpassword) {
 	document.getElementById("uCurrentPass").value = vpassword;
 }
 
+$.fn.form.settings.rules.myCustomRule = function(param) {
+	var re = new RegExp("^[a-zA-Z .]+$");
+	if(param != ""){
+		if (re.test(param))
+		    return true;
+	}
+	else
+		return true;
+}
+
+$.fn.form.settings.rules.myCustomNewPass = function(param) {
+	if(param != ""){
+		if (param.length >= 8)
+		    return true;
+	}
+	else
+		return true;
+}
+
+$.fn.form.settings.rules.myCustomConfPass = function(param, param1) {
+	if(param != ""){
+		if (param == param1)
+		    return true;
+	}
+	else
+		return true;
+}
+
 // unsorted
 function edit() {
+	$('.ui.form').form({
+		fields : {
+			uF_name : {
+				identifier : 'uF_name',
+				rules : [{
+					type : 'empty',
+					prompt : 'First Name must not be blank.'
+				}, {
+					type : 'myCustomRule[uF_name]',
+					prompt : 'First Name must only contain letters.'
+				}]
+			},
+			uL_name : {
+				identifier : 'uL_name',
+				rules : [ {
+					type : 'empty',
+					prompt : 'Last Name must not be blank.'
+				}, {
+					type : 'myCustomRule[uL_name]',
+					prompt : 'Last Name must only contain letters.'
+				}]
+			}
+		}
+	});
+	
 	$('#saveBtn').show();
 	$('.displayFirst').hide();
 	$('.displaySec').show();
@@ -684,7 +713,7 @@ function changePassword() {
 					type : 'empty',
 					prompt : 'Please input your new password'
 				}, {
-					type : 'length[8]',
+					type : 'myCustomNewPass[uNewPass]',
 					prompt : 'New password must be at least 8 characters'
 				}, {
 					type : 'different[uCurrentPass]',
@@ -697,7 +726,7 @@ function changePassword() {
 					type : 'empty',
 					prompt : 'Please confirm your new password'
 				}, {
-					type : 'match[uNewPass]',
+					type : 'myCustomConfPass[uCNewPass, uNewPass]',
 					prompt : 'Please confirm your new password'
 				} ]
 			}
@@ -772,6 +801,16 @@ function tablePayment(acct_id) {
 		balance();
 }
 
+$.fn.form.settings.rules.myCustomPymnt = function(param) {
+	var re = new RegExp("^\\s*(?=.*[1-9])\\d*(?:\\.\\d{1,2})?\\s*\$");
+	if(param != ""){
+		if (re.test(param))
+		    return true;
+	}
+	else
+		return true;
+}
+
 var amt;
 var acct_type;
 function addPayment(account_id, acct_name, amts, type) {
@@ -801,7 +840,7 @@ function addPayment(account_id, acct_name, amts, type) {
 					type : 'notExactly[0]',
 					prompt : 'Amount must be greater than 0.00 PHP'
 				},{
-					type : 'regExp[/^\\s*(?=.*[1-9])\\d*(?:\\.\\d{1,2})?\\s*\$/]',
+					type : 'myCustomPymnt[pmAmount]',
 					prompt : 'Amount must only contain valid decimal numbers.'
 				}]
 			}
@@ -811,12 +850,16 @@ function addPayment(account_id, acct_name, amts, type) {
 
 function pymntAdded() {
 	var smt = document.getElementById("pmAmount").value;
-	if(!isNaN(smt.replace(/,/g, ''))){
+	if(!isNaN(smt)){
+		console.log("yyehyrt " + smt);
 		setTimeout(function(){
-			amt -= parseFloat(smt.replace(/,/g, ''));
+			amt -= smt;
+			console.log(amt);
 			if(amt >= 0) {
+				console.log("yyehyrt2");
 				$('#errr').hide();
 				if($('.ui .error .message').length == 0){
+					console.log("yyehyrt3");
 					$("#yeah").load(location.href + " #yeah", function() {});
 					var appTable = document.getElementById("paymentsTable");
 					var row;
@@ -848,6 +891,10 @@ function pymntAdded() {
 			else if(amt < 0){
 				amt += parseFloat(smt.replace(/,/g, ''));
 				$('#errr').show();
+			}
+			else {
+				amt += parseFloat(smt.replace(/,/g, ''));
+				console.log("yyehyrt4 " + amt);
 			}
 		}, 300);
 	}
