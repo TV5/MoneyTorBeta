fb1a1eb1cdc9a53a6d06627ac17bde1fdaa96a50
Ivan Wesley Chua <ivanwesleychua@yahoo.com>
2016-02-15 20:39:34 +0800

    updates

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,9 +3,7 @@ import moneytor.*
 class BootStrap {
 
     def init = { servletContext ->
-		if (!User.count()) {
-			new User(f_name: "adminuser", l_name: "adminuser", username: "adminuser", password: "adminuser".encodeAsPassword(), type: "A", status: 1, updated_on: new Date(), updated_by: 1).save(failOnError: true)
-		}
+		
 	}
     def destroy = {
 		getUserByUsername("adminuser").delete()
